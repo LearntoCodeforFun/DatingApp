a93420c2a7721736bee5bfd666b9a387d5ab501a
LearntoCodeforFun <evaluator.you@gmail.com>
2019-02-24 21:18:31 -0500

    end section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
   model: any = {};
   logginUserName = 'User';
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
     this.logginUserName = this.authService.getLoginUserName();
@@ -21,10 +22,13 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       this.logginUserName = this.authService.getLoginUserName();
       this.alertify.success('logged in successfully');
+     
     }, error => {
       this.alertify.error(error);
       console.log('failed to login');
       console.log(error);
+    } , () => {
+      this.router.navigate(['/members']);
     });
     console.log(this.model);
   }
@@ -38,6 +42,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
     console.log('logged out');
   }
 }
